bae60dccc1b712c269147e993e8284ef27dfe3dd
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2019-06-03 01:07:12 +0200

    normal dice withour reroll

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
-type count struct {
-	one, two, three, four, five, six int
-}
-type results struct {
-	count
-	total int
+type faceCount [6]int
+type results [3]int
+type failOrSucess [6]int
+type dicesRoll struct {
+	faceCount
+	failOrSucess
+	results
+	total         int
+	reroll        int
+	resultPerCent [3]float64
 }
 
 func main() {
-	var myResults results
+	var myResults dicesRoll
+	myResults.total = 1000
+	myResults.failOrSucess[0] = 1
+	myResults.failOrSucess[1] = 1
+	myResults.failOrSucess[5] = 2
 
-	fmt.Println(myResults)
+	for index := 0; index < myResults.total; index++ {
+		randNum := rand.Intn(6)
+		myResults.faceCount[randNum]++
+		if myResults.failOrSucess[randNum] == 0 {
+			myResults.results[0]++
+		}
+		if myResults.failOrSucess[randNum] == 1 {
+			myResults.results[1]++
+		}
+		if myResults.failOrSucess[randNum] == 2 {
+			myResults.results[2]++
+		}
+	}
+
+	myResults.resultPerCent[0] = math.Round(100 * float64(myResults.results[0]) / float64(myResults.total))
+	myResults.resultPerCent[1] = math.Round(100 * float64(myResults.results[1]) / float64(myResults.total))
+	myResults.resultPerCent[2] = math.Round(100 * float64(myResults.results[2]) / float64(myResults.total))
+
+	fmt.Println(myResults.resultPerCent)
+
 }
